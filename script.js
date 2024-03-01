0fee20b7f53528ac0c628084aee637a3a34dfcf8
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-03-01 16:58:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-function firstWord(str) {
-   if (!str || str.indexOf(' ') === -1) {
-    return str;
+function firstWord(s) {
+     if (s === '') {
+    return s; // Return the entire string if it's empty
   }
-	return str.substring(0,str.indexOf(' '));
+
+  let firstWord = '';
+  let foundSpace = false;
+
+  for (let i = 0; i < s.length; i++) {
+    const currentChar = s[i];
+
+    if (currentChar === ' ' && !foundSpace) {
+      foundSpace = true; // Set flag when the first space is encountered
+    } else if (!foundSpace) {
+      firstWord += currentChar; // Append characters to firstWord until a space is found
+    }
+  }
+
+  return firstWord || s;
 }
 
-// Get input from the user:
-const inputString = prompt("Enter String:");
+// Do not change the code below
 
-// Call the function and display the result:
-alert(firstWord(inputString));
+const s = prompt("Enter String:");
+alert(firstWord(s));
