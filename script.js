a676256f479cdfbca7d2166ee9343abdf38c5e16
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-03-01 18:27:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,11 @@
 function firstWord(s) {
-     if (s === '') {
-    return s; // Return the entire string if it's empty
-  }
-
-  let firstWord = '';
-  let foundSpace = false;
-
-  for (let i = 0; i < s.length; i++) {
-    const currentChar = s[i];
-
-    if (currentChar === ' ' && !foundSpace) {
-      foundSpace = true; // Set flag when the first space is encountered
-    } else if (!foundSpace) {
-      firstWord += currentChar; // Append characters to firstWord until a space is found
-    }
-  }
-
-  return firstWord || s;
+  // your code here
+	let res = s.trim().split(" ");
+	return res[0];
+	
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+const s = prompt("Enter String:")
 alert(firstWord(s));
